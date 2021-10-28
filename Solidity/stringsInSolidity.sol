3bc1eeb241786e306f8e80e28a8feddd2670ce13
Andrea Chello <42430017+andreachello@users.noreply.github.com>
2021-10-28 01:49:23 +0100

    Update stringsInSolidity.sol

--- Solidity/stringsInSolidity.sol
+++ Solidity/stringsInSolidity.sol
@@ -4,6 +4,7 @@ contract stringsInSolidity {
     
     string greetings = "Hello";
     
+    // strings have to be saved to memory (like RAM which will be erased at each function call)
     function sayHello() public view returns(string memory) {
         return greetings;
     }
@@ -12,6 +13,7 @@ contract stringsInSolidity {
         greetings = _change;
     }
     
+    // Need to convert the string into bytes in Solidity in order to get the length of the string literal
     function getChar() public view returns(uint) {
         // convert strings to bytes
         bytes memory stringToBytes = bytes(greetings);
