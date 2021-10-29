7c89e0bfc841451cdcc39224671109d5b1925ed8
Andrea Chello <42430017+andreachello@users.noreply.github.com>
2021-10-29 17:45:31 +0100

    Create functionOverloading.sol

---
+++ Solidity/functionOverloading.sol
@@ -0,0 +1,36 @@
+pragma solidity >= 0.7.0 < 0.9.0;
+
+/*
+
+Function Overloading:
+
+is when we can have multiple definitions for the same function in the same scope.
+
+The definition of the function must differ from each other by the types and/or
+number of arguments in the argument list.
+
+We cannot overload function declarations that differ only by return type.
+
+*/
+
+contract FunctionOverloading {
+    
+    // same named function but with different input types
+    
+    function x(bool isTrue) public {
+        
+    }
+    
+    function x(uint number) public {
+        
+    }
+    
+    // Calculations with function Overloading
+    function sum(uint a, uint b) public pure returns(uint){
+        return a+b;
+    }
+    
+    function sum(uint a, uint b, uint c) public pure returns(uint) {
+        return a+b+c;
+    }
+}
