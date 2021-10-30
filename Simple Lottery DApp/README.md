7a1e5f0d02455b28c4e1f220f404e01f66a2df38
Andrea Chello <42430017+andreachello@users.noreply.github.com>
2021-10-30 05:49:30 +0100

    Delete Simple Lottery DApp directory

--- Simple Lottery DApp/README.md
+++
@@ -1,1 +0,0 @@
-# Simple Lottery DApp with Front end in React
--- Simple Lottery DApp/lottery-react/src/web3.js
+++
@@ -1,9 +0,0 @@
-import Web3 from "web3";
-
-// remove metamask provider and use our own web3 provider
-
-window.ethereum.request({ method: "eth_requestAccounts" });
- 
-const web3 = new Web3(window.ethereum);
- 
-export default web3;
