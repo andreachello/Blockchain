862b2b8fbcb36171933ee06e112aad4f0a35fb51
Andrea Chello <42430017+andreachello@users.noreply.github.com>
2021-10-30 05:50:26 +0100

    Delete Campaign directory

--- Campaign/routes.js
+++
@@ -1,9 +0,0 @@
-const routes = require('next-routes')();
-
-routes
-    .add("/campaigns/new", "/campaigns/new")
-    .add("/campaigns/:address", "/campaigns/show")
-    .add("/campaigns/:address/requests", "/campaigns/requests/index")
-    .add("/campaigns/:address/requests/new", "/campaigns/requests/new");
-
-module.exports = routes;
